ad017abe36c5acae67e4a00db5a700529222812e
Mel Heravi <melifyit@gmail.com>
2022-06-19 17:26:12 -0400

    committed

--- src/index.js
+++ src/index.js
@@ -27,11 +27,7 @@ import "./w/js/include";
 
 import "./w/js/ext/DataTables/datatables.min.css";
 import "./w/js/ext/DataTables/FixedColumns-4.1.0/css/fixedColumns.bootstrap.min.css";
-import "./w/js/ext/DataTables/Buttons-2.2.3/css/buttons.bootstrap5.min.css";
-
-import "./w/js/ext/DataTables/datatables.min.js";
 import "./w/js/ext/DataTables/FixedColumns-4.1.0/js/dataTables.fixedColumns.min.js";
-import "./w/js/ext/DataTables/Buttons-2.2.3/js/buttons.bootstrap5.min.js";
 
 import "./w/scss/_table.scss";
 import "./w/js/table";
