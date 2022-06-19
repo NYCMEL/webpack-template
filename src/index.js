61ad5cb19718e490df3eeaf303cff65b334123e2
Mel Heravi <melifyit@gmail.com>
2022-06-19 13:42:03 -0400

    committed

--- src/index.js
+++ src/index.js
@@ -25,13 +25,16 @@ import "./w/js/tests";
 import "./w/scss/_include.scss";
 import "./w/js/include";
 
-import "/tk/lib/components/misc/webpack-template/node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "/Melify/_needed/DataTables//DataTables-1.12.1/css/dataTables.bootstrap5.min.css";
-import "/Melify/_needed/DataTables/FixedColumns-4.1.0/css/fixedColumns.bootstrap.min.css";
+import "datatables";
+//import "datatables.net-fixedcolumns";
 
-import "/tk/lib/components/misc/webpack-template/node_modules/datatables.net-fixedcolumns/css/fixedColumns.bootstrap5.scss";
-import "/Melify/_needed/DataTables/DataTables-1.12.1/js/dataTables.bootstrap5.min.js";
-import "/Melify/_needed/DataTables/FixedColumns-4.1.0/js/dataTables.fixedColumns.min.js";
+// import "/tk/lib/components/misc/webpack-template/node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "/Melify/_needed/DataTables//DataTables-1.12.1/css/dataTables.bootstrap5.min.css";
+// import "/Melify/_needed/DataTables/FixedColumns-4.1.0/css/fixedColumns.bootstrap.min.css";
+
+// import "/tk/lib/components/misc/webpack-template/node_modules/datatables.net-fixedcolumns/css/fixedColumns.bootstrap5.scss";
+// import "/Melify/_needed/DataTables/DataTables-1.12.1/js/dataTables.bootstrap5.min.js";
+// import "/Melify/_needed/DataTables/FixedColumns-4.1.0/js/dataTables.fixedColumns.min.js";
 
 import "./w/scss/_table.scss";
 import "./w/js/table";
