32c413f752008d5046d93ab64997b1ececfd8b10
Mel Heravi <melifyit@gmail.com>
2022-06-20 10:23:13 -0400

    committed

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ import "./w/scss/_include.scss";
 
         import "./w/scss/ext/_dataTables.scss";
 	import "datatables";
+	import "./w/js/ext/dataTables.fixedColumns.min.js";
 	
         import "./w/scss/_table.scss";
         import "./w/js/table";
--- src/w/req/table.tcl
+++ src/w/req/table.tcl
@@ -2,6 +2,7 @@ array set req {
     table {
         import "./w/scss/ext/_dataTables.scss";
 	import "datatables";
+	import "./w/js/ext/dataTables.fixedColumns.min.js";
 	
         import "./w/scss/_table.scss";
         import "./w/js/table";
