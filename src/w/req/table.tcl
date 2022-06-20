00add9380418923cb55db04c3f5fa0ccfc8f994d
Mel Heravi <melifyit@gmail.com>
2022-06-20 09:13:49 -0400

    committed

---
+++ src/w/req/table.tcl
@@ -0,0 +1,11 @@
+array set req {
+    table {
+        import "./w/scss/ext/_dataTables.scss";
+	import "datatables";
+	import "../node_modules/datatables.net/js/jquery.dataTables.js";
+	
+        import "./w/scss/_table.scss";
+        import "./w/js/table";
+    }
+}
+
