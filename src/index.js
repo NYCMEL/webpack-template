d002cdff4a72fc17ec86c473c563564a2ab7c1a5
Mel Heravi <melifyit@gmail.com>
2022-06-20 10:41:23 -0400

    committed

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@
 // REQUIRED BY ALL
 ////////////////////////////////////////////////////////////////
 import "bootstrap";
-import "pubsub-js";
-
 import "./w/scss/_bootstrap.scss"
 
-import "./w/scss/_wc.scss";
-import "./w/js/wc";
+import "pubsub-js"; /* PUBLISHER SUBSCRIBER */
 
-import "./w/scss/_tk.scss";
-import "./w/js/tk.js";
+import "./w/scss/_wc.scss"; /* GENERAL STYLES */
+import "./w/js/wc"; /* COMMON JS FUNCTION */
 
-import "jquery.cookie";
+import "./w/scss/_tk.scss"; /* LOADING STYLES */
+import "./w/js/tk.js"; /* DATA BINDING, LOADING ETC. */
 
-import "./w/js/include";
+import "jquery.cookie"; /* COOKIE SETTER/GETTER */
 
-import "./w/js/tests";
+import "./w/js/include"; /* ALLOW TO INCLUDE URL(S) */
+
+import "./w/js/tests"; /* COMPONENT TESTER */
 
 ////////////////////////////////////////////////////////////////
 // PER COMPONENT
@@ -26,6 +26,10 @@ import "./w/scss/_include.scss";
 		    import "./w/js/include";
 		
 
-		    import "./w/scss/_blank.scss";
-		    import "./w/js/blank";
+        import "./w/scss/ext/_dataTables.scss";
+	import "datatables";
+	import "./w/js/ext/dataTables.fixedColumns.min.js";
+	
+        import "./w/scss/_table.scss";
+        import "./w/js/table";
 
